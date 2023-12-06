89fe2f1f1b6011b29c748b79a3596855dc799592
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-12-05 19:46:01 -0500

    Fix access control and add record to database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
+
 class AuthController extends Controller
 {
     /**
@@ -39,18 +40,38 @@ class AuthController extends Controller
             ->where('username', $request->username)
             ->first();
 
-        $remember = false;
+        $remember = true;
 
         // if $user is null, then $user->access_room_911 will assinf falseto $access
 
-        if ($user == null || $user->access_room_911 == false) {
+        if ($user == null) {
             return back()->withErrors([
                 'username' => 'The provided credentials do not match our records.',
             ]);
         }
 
+        if ($user->access_room_911 == false) {
+            // save a record to the db with the user_id and status rejected
+            $record = new \App\Models\Record();
+            $record->user_id = $user->id;
+            $record->status = 'rejected';
+            $record->save();
+
+            return back()->withErrors([
+                'username' => 'You do not have access.',
+            ]);
+        }
+
+
 
         if (Auth::loginUsingId($user->id, $remember)) {
+
+            // save a record to the db with the user_id and status approve
+            $record = new \App\Models\Record();
+            $record->user_id = $user->id;
+            $record->status = 'approved';
+            $record->save();
+
             return redirect()->intended('/');
         } else {
             return back()->withErrors([
